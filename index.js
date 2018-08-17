1a8761492ca5e9e22076a01c9824b518f3af69bb
Manh Le <quang.manh@gmail.com>
2018-08-16 22:13:09 -0500

    Installed gulp, less + configuration

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-var liveServer = require("live-server");
- 
+const fs = require('fs')
+const path = require('path')
+const liveServer = require("live-server");
+const less = require('less');
+const gulp = require('gulp')
+const lessMap = require('./lessmap.json')
+
+function translate(from,to){
+	fs.readFile(from, 'utf8', function (err, data) {
+		if(err)
+			console.log(err)
+		else
+			less.render(data, (e, out) => {
+				if (e)
+					console.log(e);
+				else
+					fs.writeFile(to,data,'utf-8',()=>{})					
+			})	
+	});
+}
+
+lessMap.forEach(p=>{
+	translate(p.less,p.css);
+	gulp.watch(p.less).on("change", d => {
+		translate(p.less,p.css);
+	})	
+})
+
+gulp.watch('./src/public/app/less/*.less').on("change", d => {
+	let p = d.path.split("\\");
+	let filename = p[p.length - 1].split(".")[0];
+
+	fs.readFile(d.path, 'utf8', function (err, data) {
+		if(err)
+			console.log(err)
+		else
+			less.render(data, (e, out) => {
+				if (e)
+					console.log(e);
+				else {				
+					fs.writeFile(path.join(__dirname, "src/public/app/css", filename + ".css")
+					,out.css,'utf-8',()=>{})
+				}
+			})
+
+	});
+})
+
 var params = {
     port: 8181, // Set the server port. Defaults to 8080.
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
