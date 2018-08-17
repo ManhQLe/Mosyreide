a403d2402a049e79370ff8a3ca571a2a41b22e9c
Manh Le <quang.manh@gmail.com>
2018-08-16 23:31:39 -0500

    Finished resize canvas

--- src/content/js/main.js
+++ src/content/js/main.js
@@ -1,19 +1,25 @@
 let canvas = document.getElementById("main-canvas")
 console.log(canvas.offsetHeight)
 let mainApp = new PIXI.Application({
-    width:1,
-    heigh:1,
+    width:100,
+    height:100,
     view:canvas,
     backgroundColor:0x000000,
     antialias:true,
     resolution:1
 })
-mainApp.renderer.autoResize = true;
-mainApp.renderer.resize(canvas.offsetWidth,canvas.offsetHeight);
+let renderer = mainApp.renderer;
+
+function resize(){
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    renderer.resize(w,h);
+    renderer.view.style.width = w+"px"
+    renderer.view.style.height = h+"px"
+}
+resize();
+
 window.onresize = function(){
-    mainApp.renderer.resize(canvas.offsetWidth,canvas.offsetHeight);
-    this.console.log(canvas.offsetWidth,canvas.offsetHeight)
-    
-    this.console.log(mainApp.renderer.view.width,mainApp.renderer.view.height)
+    resize();
 }
 
