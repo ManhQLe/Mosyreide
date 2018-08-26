c51dde8e6a05eb97224d53bcf0f040401a32f654
Manh Le <quang.manh@gmail.com>
2018-08-26 00:13:47 -0500

    Drafted Drop

--- src/content/js/ce.js
+++ src/content/js/ce.js
@@ -1,7 +1,7 @@
 class CE extends mosyrejs2.Clay {
     constructor(agr) {
         super(agr)
-        let zoom = 1, pos = [0, 0], zoomRate = 0.35;
+        let zoom = 1, pos = [0, 0];
         Object.defineProperties(this, {
             "zoom": {
                 get: () => zoom,
@@ -16,10 +16,6 @@ class CE extends mosyrejs2.Clay {
                     pos = v
                     this._applyTransforms()
                 }
-            },
-            "zoomRate": {
-                get: () => zoomRate,
-                set: (v) => zoomRate = Math.max(v, 0)
             }
         })
         this.defineAgreement("canvas", document.createElement("svg"))
@@ -30,8 +26,6 @@ class CE extends mosyrejs2.Clay {
             this.zoom = agr.zoom;
         if (agr.pos !== undefined)
             this.pos = agr.pos
-        if (agr.zoomRate !== undefined)
-            this.zoomRate = agr.zoomRate;
     }
 
     init() {
@@ -39,38 +33,6 @@ class CE extends mosyrejs2.Clay {
 
         this.__.g = document.createElementNS(canvas.namespaceURI, "g");
         canvas.appendChild(this.__.g);
-        let track = this.__.track = [{
-            x: 0,
-            y: 0
-        }]
-
-        canvas.addEventListener("wheel", (e) => {
-            let m = this._getMouse(e)
-            let rate = this.zoomRate;
-            let nz = this.zoom + ((e.deltaY < 0) ? rate : -rate);
-            this.zoomTo(nz, m)
-        })
-
-        canvas.addEventListener("mousedown", (e) => {
-            let m = this._getMouse(e)
-            track.x = m[0];
-            track.y = m[1];
-            console.log(m)
-            console.log(this.view2World(m))
-        })
-
-        canvas.addEventListener("mousemove", (e) => {
-            let m = this._getMouse(e)
-            if (e.buttons == 1) {
-                let dv = this.toWorldScale([track.x - m[0], track.y - m[1]])
-                let pos = this.pos;
-                pos[0] += dv[0];
-                pos[1] += dv[1];
-                track.x = m[0];
-                track.y = m[1];
-                this.pos = pos;
-            }
-        })
     }
 
     zoomTo(z, atPx) {
@@ -151,11 +113,8 @@ class CE extends mosyrejs2.Clay {
         this.__.g.setAttribute("transform", transforms.join(" "));
     }
 
-    _getMouse(e){
-        var rect = this.agreement.canvas.getBoundingClientRect();
-        var x = e.clientX - rect.left; //x position within the element.
-        var y = e.clientY - rect.top;  //y position within the element.
-        return [x,y]
+    createElement(name){
+        return document.createElementNS(this.agreement.canvas.namespaceURI, name)
     }
 
     static getScale(wp, z) {
--- src/content/js/main.js
+++ src/content/js/main.js
@@ -1,59 +1,134 @@
+
 let ce = new CE({
-    canvas:document.getElementById("main-canvas"),
-    //zoom:2,
-    zoomRate:.35
+    canvas:document.getElementById("main-canvas") 
 })
 
-d3.select(ce.__.g).append("circle")
-.attr("cx",50)
-.attr("cy",50)
-.attr('r',10)
-.attr("fill","#f1c40f")
+var entities = [];
+let zoomRate = .15;
+let canvas = ce.agreement.canvas;
+let track = {
+    x: 0,
+    y: 0
+}
+let state = 0
 
-d3.select(ce.__.g).append("rect")
-.attr("x",0)
-.attr("y",0)
-.attr("width",100)
-.attr("height",120)
-.attr("stroke","#ecf0f1")
-.attr("fill","none")
+function createRClay(pos){
+    let g = ce.createElement("g")
+    g.setAttribute("transform",`translate(${pos[0]},${pos[1]})`)
+    d3.select(g).append("rect")
+    .attr("width",100)
+    .attr("height",100)
+    .attr("fill","gray")
 
-d3.select(ce.__.g).append("rect")
-.attr("x",0)
-.attr("y",0)
-.attr("width",100)
-.attr("height",100)
-.attr("stroke","#e74c3c")
-.attr("fill","none")
+    var clay = new mosyrejs2.RClay({
+        _vessel:g,
 
-d3.select(ce.__.g).append("rect")
-.attr("x",50)
-.attr("y",50)
-.attr("width",50)
-.attr("height",50)
-.attr("stroke","#f1c40f")
-.attr("fill","none")
+    })
+    entities.push(clay);
+    return clay;
+    
+}
 
 
-// d3.select(ce.__.g).append("circle")
-// .attr("cx",50)
-// .attr("cy",100)
-// .attr('r',50)
-// .attr("fill","#e74c3c")
+function getMouse(e){
+    var rect = canvas.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element.
+    return [x,y]
+}
 
-d3.select(ce.__.g).append("rect")
-.attr("x",50)
-.attr("y",50)
-.attr("width",50)
-.attr("height",100)
-.attr("stroke","#9b59b6")
-.attr("fill","none")
+d3.select(canvas).on("drop",(e = d3.event)=>{
+    e.preventDefault();    
+    let p = ce.view2World(getMouse(e));
+    console.log(p)
+    let c = createRClay(p);    
+    ce.__.g.append(c.agreement._vessel);
 
-// ce.zoomWRect({
-//     x:50,
-//     y:50,
-//     w:50,
-//     h:100
-// },1)
-//ce.toCenter();
-// ce.lookAt([0,0])
+
+})
+
+canvas.addEventListener("wheel", (e) => {
+    let m = getMouse(e)    
+    let nz = ce.zoom * (1 + ((e.deltaY < 0) ? zoomRate : -zoomRate));
+    ce.zoomTo(nz, m)
+})
+
+canvas.addEventListener("mousedown", (e) => {
+    let m = getMouse(e)
+    track.x = m[0];
+    track.y = m[1];
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    let m = getMouse(e)
+    if (e.buttons == 1) {
+        let dv = ce.toWorldScale([track.x - m[0], track.y - m[1]])
+        let pos = ce.pos;
+        pos[0] += dv[0];
+        pos[1] += dv[1];
+        track.x = m[0];
+        track.y = m[1];
+        ce.pos = pos;
+    }
+})
+
+function Render(){
+    d3.select(ce.__.g).append("circle")
+    .attr("cx",50)
+    .attr("cy",50)
+    .attr('r',10)
+    .attr("fill","#f1c40f")
+    
+    d3.select(ce.__.g).append("rect")
+    .attr("x",0)
+    .attr("y",0)
+    .attr("width",100)
+    .attr("height",120)
+    .attr("stroke","#ecf0f1")
+    .attr("fill","none")
+    
+    d3.select(ce.__.g).append("rect")
+    .attr("x",0)
+    .attr("y",0)
+    .attr("width",100)
+    .attr("height",100)
+    .attr("stroke","#e74c3c")
+    .attr("fill","none")
+    
+    d3.select(ce.__.g).append("rect")
+    .attr("x",50)
+    .attr("y",50)
+    .attr("width",50)
+    .attr("height",50)
+    .attr("stroke","#f1c40f")
+    .attr("fill","none")
+    
+    
+    // d3.select(ce.__.g).append("circle")
+    // .attr("cx",50)
+    // .attr("cy",100)
+    // .attr('r',50)
+    // .attr("fill","#e74c3c")
+    
+    d3.select(ce.__.g).append("rect")
+    .attr("x",50)
+    .attr("y",50)
+    .attr("width",50)
+    .attr("height",100)
+    .attr("stroke","#9b59b6")
+    .attr("fill","none")
+    
+    // ce.zoomWRect({
+    //     x:50,
+    //     y:50,
+    //     w:50,
+    //     h:100
+    // },1)
+    //ce.toCenter();
+    // ce.lookAt([0,0])
+}
+
+Render();
+
+
+
